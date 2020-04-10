cab284ada8e579df2e23115d73eb7b09c3c0112f
liguoqing <lgq0218@gmail.com>
2020-04-10 21:44:49 +0800

    SpringBoot单元测试

--- spring-boot-test/src/test/java/com/dobby/web/UserControllerTest.java
+++ spring-boot-test/src/test/java/com/dobby/web/UserControllerTest.java
@@ -1,21 +1,39 @@
 package com.dobby.web;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@WebMvcTest(UserController.class)
 public class UserControllerTest {
 
-
+    @Autowired
     private MockMvc mockMvc;
 
 
 
     @Test
-    void listUser() {
+    void listUser() throws Exception {
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"));
+    }
+
+
+    @Test
+    public void hello() {
+
     }
 
 
